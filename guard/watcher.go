1b5dcb600afb9a7068420d6472abcd5245bf1e9e
Istreb4k <rakitin.dmit@gmail.com>
2021-11-19 18:20:19 +0700

    validators for the latest block

--- guard/watcher.go
+++ guard/watcher.go
@@ -265,7 +265,7 @@ func (w *Watcher) handleEventNewBlock(result ctypes.ResultEvent) (err error) {
 	w.latestBlock = event.Block.Height
 	w.latestBlockTime = event.Block.Time
 
-	_, e := w.client.Validators(&event.Block.Height, 0, 1000)
+	_, e := w.client.Validators(nil, 0, 1000)
 	if e != nil {
 		w.logger.Error(e.Error())
 
